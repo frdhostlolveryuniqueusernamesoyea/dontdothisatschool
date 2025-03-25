b7e5aa79c58bf337d2166e8fe21068b9bbd49ebc
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-03-25 21:59:45 +0100

    afk

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ function CopyBtn(copy) {
     navigator.clipboard.writeText(copy);
     alert("Copied!");
 }
+
+function ExpandInfo() {
+    var x = document.getElementById("info");
+    if (x.style.display === "none") {
+        document.getElementById('expandinfo').innerText = "Hide";
+        x.style.display = "block";
+    } else {
+        document.getElementById('expandinfo').innerText = "About";
+        x.style.display = "none";
+    }
+}
